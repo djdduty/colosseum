79f03023f907e2859b296014beee7508424cf48c
Jordan Duty <djdduty@gmail.com>
2017-08-09 00:05:31 -0700

    Basic mongo usage

--- colosseum/web/application.py
+++ colosseum/web/application.py
@@ -2,8 +2,15 @@
 
 
 from web.core import Application
+from web.ext.db import DatabaseExtension
+from web.db.mongo import MongoDBConnection
 
 from colosseum.web.controller import Controller
 
 
-application = Application(Controller)
+application = Application(
+		Controller,
+		extensions=[
+				DatabaseExtension(default=MongoDBConnection('mongodb://127.0.0.1/test')),
+			],
+	)
--- colosseum/web/controller.py
+++ colosseum/web/controller.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+import random
+from bson import json_util as json
+
 
 class Controller(object):
 	def __init__(self, context):
@@ -7,3 +10,12 @@ class Controller(object):
 
 	def __call__(self):
 		return "Hello, World!"
+
+	def mongoi(self):
+		rand = random.randint(0, 100)
+		self._ctx.db.test.insert_one({'test': rand})
+		return json.dumps(self._ctx.db.test.find_one({'test': rand}))
+
+	def mongor(self):
+		results = [element for element in self._ctx.db.test.find()]
+		return json.dumps(results)
